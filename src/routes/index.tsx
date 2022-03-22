2acf54daa65dc6eb7c36ea5ed500265de66896fc
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-03-21 21:17:05 -0400

    feat: implementação da página Not Found.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from '../App';
 import { Panel } from '../pages/Panel';
+import { NotFoundPage } from '../pages/NotFoundPage';
 import { TransactionsProvider } from '../hooks/useTransactions';
 import { AuthenticationProvider } from '../hooks/useAuthentication';
 
@@ -12,6 +13,7 @@ export const Router = () => {
           <Routes>
             <Route path='/' element={< App />} />
             <Route path='/panel' element={ <Panel />} />
+            <Route path='*' element={ <NotFoundPage />} />
           </Routes>
         </TransactionsProvider>
       </AuthenticationProvider>
